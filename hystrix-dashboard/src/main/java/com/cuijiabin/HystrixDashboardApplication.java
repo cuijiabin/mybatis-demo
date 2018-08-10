47ae5a9d4f3ff8ef3db19996adc5dc56b616abcf
cuijiabin <cuijia_bin@126.com>
2018-08-10 16:20:06 +0800

    熔断监控

---
+++ hystrix-dashboard/src/main/java/com/cuijiabin/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.cuijiabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * cuijiabin
+ */
+@EnableHystrixDashboard
+@SpringCloudApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
